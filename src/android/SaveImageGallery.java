465da392adfbd818e4f3228592f1201e26aa76e9
fernando.hieda <fernando.hieda@shx0496.shxinfor.local>
2017-05-07 17:36:44 -0300

    a

--- src/android/SaveImageGallery.java
+++ src/android/SaveImageGallery.java
@@ -184,7 +184,7 @@ public class SaveImageGallery extends CordovaPlugin {
                 return retVal;
             }
             
-            //Log.e("SaveImageToGallery", "folder: " + folder.getAbsolutePath());
+            Log.e("SaveImageToGallery", "folder: " + folder.getAbsolutePath());
             
             File nomediaFile = new File(folder, ".nomedia");
 
